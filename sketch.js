86054df1694c58483857b262b9d918fa3eb98699
Vezil <SzymonWojaczek@wp.pl>
2021-07-24 18:45:44 +0200

    add pig class and gravity'

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let pig;
+
 function setup() {
     createCanvas(400, 400);
+
+    pig = new Pig();
+}
+
+function keyPressed() {
+    if (key === ' ') {
+        pig.jump();
+    }
 }
 
 function draw() {
     background(220);
+
+    pig.show();
+    pig.move();
 }
