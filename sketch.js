ad71de6cc2ecb57f7d607ea9d459b0fa6c360a41
Vezil <44679435+Vezil@users.noreply.github.com>
2021-08-23 16:54:08 +0200

    feature/counter (#2)
    * add basic counter
    
    * add winking background

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
+const STAGE_WIDTH = 1280;
+const STAGE_HEIGTH = 720;
+
+const obstacles = [];
+const passedObstacles = [];
+
 let pig;
 let pigImage;
 let obstacleImage;
 let backgroundImage;
-let obstacles = [];
+let counter;
+let counterValue = 0;
 
 function preload() {
     pigImage = loadImage('./assets/PigCharacter.png');
@@ -11,9 +18,12 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(1280, 720);
+    createCanvas(STAGE_WIDTH, STAGE_HEIGTH);
 
     pig = new Pig();
+    counter = new Counter();
+
+    addEventListeners();
 }
 
 function keyPressed() {
@@ -23,23 +33,46 @@ function keyPressed() {
 }
 
 function draw() {
-    if (random(1) < 0.005) {
+    if (random(1) < 0.009) {
         obstacles.push(new Obstacle());
     }
 
     background(backgroundImage);
 
-    pig.show();
+    pig.render();
+    counter.render(counterValue);
+
     pig.move();
 
-    obstacles.forEach(obstacle => {
-        obstacle.show();
+    obstacles.forEach((obstacle, index) => {
+        obstacle.render();
         obstacle.move();
 
         if (pig.hits(obstacle)) {
-            console.log('GAME OVER');
-
             noLoop();
         }
+
+        const isObstaclePassed = passedObstacles.includes(obstacles[index]);
+
+        if (obstacles[index].x < pig.x && !isObstaclePassed) {
+            passedObstacles.push(obstacles[index]);
+            counterValue++;
+        }
     });
+}
+
+function wink() {
+    const pigWithClosedEyes = document.getElementsByClassName(
+        'pig-with-closed-eyes'
+    )[0];
+
+    pigWithClosedEyes.classList.add('show');
+
+    setTimeout(() => {
+        pigWithClosedEyes.classList.remove('show');
+    }, 700);
+}
+
+function addEventListeners() {
+    setInterval(wink, 3000);
 }
