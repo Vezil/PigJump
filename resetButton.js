7f6010cffa99d9cd0227c1af4233095f7187dc57
Vezil <SzymonWojaczek@wp.pl>
2021-11-16 19:50:24 +0100

    reset button separate css file

--- resetButton.js
+++ resetButton.js
@@ -4,26 +4,7 @@ class ResetButton {
     render() {
         const resetButton = createButton('reset');
 
-        const bgColor = color(14, 98, 81);
-        const textColor = color(229, 152, 102);
-        const borderRadius = '5px';
-        const width = '200px';
-        const padding = '1em 2em';
-        const letterSpacing = '5px';
-        const fontSize = '1.1rem';
-        const cursor = 'pointer';
-
-        resetButton.style('background-color', bgColor);
-        resetButton.style('background-color&hover', textColor);
-        resetButton.style('color', textColor);
-        resetButton.style('border-radius', borderRadius);
-        resetButton.style('width', width);
-        resetButton.style('padding', padding);
-        resetButton.style('letter-spacing', letterSpacing);
-        resetButton.style('font-size', fontSize);
-        resetButton.style('cursor', cursor);
-
-        resetButton.position(1200, 870);
+        resetButton.position(1250, 840);
 
         resetButton.addClass('reset-button');
 
